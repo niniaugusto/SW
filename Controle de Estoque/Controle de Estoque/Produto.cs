ac8912d5427dff3a21d1b6710b788220a6bb40c9
Nicolas Augusto <125036938+niniaugusto@users.noreply.github.com>
2024-05-03 23:41:01 -0300

    Add files via upload

---
+++ Controle de Estoque/Controle de Estoque/Produto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioVideo
+{
+    class Produto
+    {
+        private int codigo;
+        private string nome;
+        private int estoque;
+        private Double valor;
+
+        public Produto(int codigo, string nome, int estoque, double valor)
+        {
+            this.codigo = codigo;
+            this.nome = nome;
+            this.estoque = estoque;
+            this.valor = valor;
+        }
+
+        public void setCodigo(int codigo)
+        {
+            this.codigo = codigo;
+        }
+        public int getCodigo()
+        {
+            return codigo;
+        }
+        public void setNome(String nome)
+        {
+            this.nome = nome;
+        }
+        public String getNome()
+        {
+            return nome;
+        }
+        public void setValor(Double valor)
+        {
+            this.valor = valor;
+        }
+        public Double getValor()
+        {
+            return valor;
+        }
+
+        public void entrada(int qtd)
+        {
+            estoque = estoque + qtd;
+            Console.WriteLine("Entrada de " + qtd);
+        }
+        public void venda(int qtd)
+        {
+            estoque = estoque - qtd;
+            Console.WriteLine("Venda de " + qtd);
+        }
+        public void imprimir()
+        {
+            Console.WriteLine("Dados do produto");
+            Console.WriteLine("Código: " + codigo);
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Estoque: " + estoque);
+            Console.WriteLine("Valor: " + valor);
+        }
+    }
+}
---
+++ Controle de Estoque/Controle de Estoque/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ExercicioVideo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto(1, "Notebook", 0, 2300);
+            p.imprimir();
+            p.entrada(20);
+            p.imprimir();
+            p.venda(5);
+            p.imprimir();
+        }
+    }
+}
